57bc5e1f76e8b652a22016bc972894cd71e1ca11
funduval <fundaduval@gmail.com>
2017-08-19 23:58:07 -0400

    initial files

---
+++ controllers/controller.js
@@ -0,0 +1,154 @@
+// Node Dependencies
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var request = require('request'); // for web-scraping
+var cheerio = require('cheerio'); // for web-scraping
+
+// Import the Comment and Article models
+var Comment = require('../models/Comment.js');
+var Article = require('../models/Article.js');
+
+// Index Page Render (first visit to the site)
+router.get('/', function (req, res){
+
+  // Scrape data
+  res.redirect('/scrape');
+
+});
+
+
+// Articles Page Render
+router.get('/articles', function (req, res){
+
+  // Query MongoDB for all article entries (sort newest to top, assuming Ids increment)
+  Article.find().sort({_id: -1})
+
+    // But also populate all of the comments associated with the articles.
+    .populate('comments')
+
+    // Then, send them to the handlebars template to be rendered
+    .exec(function(err, doc){
+      // log any errors
+      if (err){
+        console.log(err);
+      } 
+      // or send the doc to the browser as a json object
+      else {
+        var hbsObject = {articles: doc}
+        res.render('index', hbsObject);
+        // res.json(hbsObject)
+      }
+    });
+
+});
+
+
+// Web Scrape Route
+router.get('/scrape', function(req, res) {
+
+  // First, grab the body of the html with request
+  request('http://www.nytimes.com/', function(error, response, html) {
+
+    // Then, load html into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+
+    // This is an error handler for the NY Times website only, they have duplicate articles for some reason...
+    var titlesArray = [];
+
+    // Now, grab every everything with a class of "inner" with each "article" tag
+    $('div .collection headlines').each(function(i, element) {
+
+        // Create an empty result object
+        var result = {};
+
+        // Collect the Article Title (contained in the "h2" of the "header" of "this")
+        result.title = $(this).children("ul .theme-news-headlines").children("").text().trim() + ""; //convert to string for error handling later
+
+        // Collect the Article Link (contained within the "a" tag of the "h2" in the "header" of "this")
+        // result.link = $(this).children('h2').children('a').attr('href').trim();
+
+        
+    });
+
+    // Redirect to the Articles Page, done at the end of the request for proper scoping
+    res.redirect("/articles");
+
+  });
+
+});
+
+
+// Add a Comment Route - **API**
+router.post('/add/comment/:id', function (req, res){
+
+  // Collect article id
+  var articleId = req.params.id;
+  
+  // Collect Author Name
+  var commentAuthor = req.body.name;
+
+  // Collect Comment Content
+  var commentContent = req.body.comment;
+
+  // "result" object has the exact same key-value pairs of the "Comment" model
+  var result = {
+    author: commentAuthor,
+    content: commentContent
+  };
+
+  // Using the Comment model, create a new comment entry
+  var entry = new Comment (result);
+
+  // Save the entry to the database
+  entry.save(function(err, doc) {
+    // log any errors
+    if (err) {
+      console.log(err);
+    } 
+    // Or, relate the comment to the article
+    else {
+      // Push the new Comment to the list of comments in the article
+      Article.findOneAndUpdate({'_id': articleId}, {$push: {'comments':doc._id}}, {new: true})
+      // execute the above query
+      .exec(function(err, doc){
+        // log any errors
+        if (err){
+          console.log(err);
+        } else {
+          // Send Success Header
+          res.sendStatus(200);
+        }
+      });
+    }
+  });
+
+});
+
+
+
+
+// Delete a Comment Route
+router.post('/remove/comment/:id', function (req, res){
+
+  // Collect comment id
+  var commentId = req.params.id;
+
+  // Find and Delete the Comment using the Id
+  Comment.findByIdAndRemove(commentId, function (err, todo) {  
+    
+    if (err) {
+      console.log(err);
+    } 
+    else {
+      // Send Success Header
+      res.sendStatus(200);
+    }
+
+  });
+
+});
+
+
+// Export Router to Server.js
+module.exports = router;
